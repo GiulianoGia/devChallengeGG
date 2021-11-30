bf5cc3775dba7cf146a59f217aa1a61f9f5124eb
Giuliano Gianola <68944432+GiulianoGia@users.noreply.github.com>
2021-11-30 08:44:07 +0100

    Done Challenge
    Honestly, it has taken a much longer time than the expected 30 minutes in the description. But in the end, I have hopefully made it :)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
 export default {
-  name: 'App',
-  components: {}
-}
+  data() {
+    return {
+      active: false,
+      input: "",
+      items: [],
+    };
+  },
+  methods: {
+    deleteItem() {
+      var input = document.getElementById('inputfield')
+      console.log('delete')
+      this.input = '';
+    },
+    selectItem(item) {
+      var input = document.getElementById("inputfield");
+      input.value = item;
+    },
+    returnResult: function (results) {
+      this.items = results;
+    },
+    fetchApi: function () {
+      
+      if (inputfield.value == "") {
+        this.active = false;
+        this.items = [];
+      }
+      else {
+        this.active = true;
+        fetch(`http://transport.opendata.ch/v1/locations?query=${this.input}`)
+          .then((response) => response.json())
+          .then((data) => this.returnResult(data.stations));
+      }
+    },
+  },
+};
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
-  
+  <div class="main">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+      rel="stylesheet"
+    />
+    <div class="inputfield_container">
+      <label v-if="active" id="label" for="inputfield">Von</label>
+      <input
+        placeholder="Von"
+        id="inputfield"
+        type="text"
+        @input="fetchApi"
+        v-model="input"
+      />
+      <button v-if="active" v-on:click="deleteItem(), active = false" id="delete">
+        <img class="img" src="./assets/clear.svg"/>
+      </button>
+      <button id="delete" v-else></button>
+    </div>
+    <ul id="item_container" v-if="active">
+      <li id="item" v-for="item in items" :key="item.name">
+        <a v-on:click="selectItem(item.name)"><img id="img" img src="./assets/train.svg" />{{ item.name }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
 <script src="./App.js" />
 <style src="./App.css" />
